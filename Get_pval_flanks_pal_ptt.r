ea43de9056a8b2f2e9c3975a7044b84fb683a215
Andrey Kulakovskiy <kulakovskiy@gmail.com>
2014-03-23 01:39:55 +0400

    Cleanup code and remove error in code which generates random flanks

--- Get_pval_flanks_pal_ptt.r
+++ Get_pval_flanks_pal_ptt.r
@@ -2,21 +2,32 @@ require(IRanges)
 require(parallel)
 require (data.table)
 require(plyr)
+require(RCurl)
 
-feature <- "transposase"
-work.env <- "bacteria_S6-15_L0-10_M1"
-cat(paste(Sys.time(), "This R script has started!", sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)
+#====== List of parameters: ======
+pal.path <- ("D:/DNAPUNCTUATION/ftp/bacteria_S6-15_L0-10_M1") #folder with .pal.cleaned files
+ptt.path <- "D:/DNAPUNCTUATION/all.ptt/" #folder with .ptt files
+feature = 'transposase' #feature in genes markdown(PTT files)
+genlenfile = 'D:/smpl_files/genome.length.txt'    
+flank_length = 50  #size of flank
+tag = 'TEST' #using for tagging file
+#=================================
 
-pal.path <- paste0("D:/DNAPUNCTUATION/ftp/", work.env, "/")
-ptt.path <- "D:/DNAPUNCTUATION/all.ptt/"
+#Now let's create names of log.file, results and final summary
+palindrome.type = basename(pal.path) # Example: /ftp/bacteria_S6-15_L0-10_M1 ===> bacteria_S6-15_L0-10_M1  
+main.dir = getwd()# Script executing dir
+results.path =file.path(main.dir, 'pal_ptt_results')
+dir.create(results.path, showWarnings=F) #path to results folder for current work
+cur.res.name = paste(tag, palindrome.type, feature, 'flank', flank_length,'pal_ptt', sep = '_') #common name for current work
 
-# pal.path <- paste0("D:/DNAPUNCTUATION/ftp/", work.env, "/")
-# ptt.path <- "D:/DNAPUNCTUATION/all.ptt/"
+log.file = file.path(results.path, paste0(cur.res.name,'.log'))
+out.file = file.path(results.path, paste0(cur.res.name,'.out'))
+all.file = file.path(results.path, paste0(cur.res.name,'.summary'))
 
+#get all needed data
 pal.files <- list.files(path=pal.path, pattern="cleaned$", recursive=T, full.names=F)
 ptt.files <- list.files(path=ptt.path, pattern="ptt$", recursive=T, full.names=F)
-
-genlen.all <- read.table(file="D:/smpl_files/genome.length.txt", header=F, sep="\t")
+genlen.all <- read.table(file=genlenfile, header=F, sep="\t")
 
 #extract NC ID's from .ptt files
 m <- regexpr("\x2F(.+?).ptt", x<-ptt.files, perl=T)
@@ -34,40 +45,39 @@ pal.df <- data.frame(pal.file = paste0(pal.path,pal.files), nc.id = pal.nc.id)
 
 #data.frame with both .ptt and pal.cleaned files, will be passed to our parallel env.
 all.df <- merge(ptt.df, pal.df,by = intersect("nc.id", "nc.id"))
+#i'm not sure that 3 lines below needed...
 all.df$nc.id <-as.character(all.df$nc.id)
 all.df$ptt.file <-as.character(all.df$ptt.file)
 all.df$pal.file <-as.character(all.df$pal.file)
 
-#add new one feature
-coverage.data <- read.table('s615.f50.trp.aroundone.txt', header=T, sep='\t')
-
-all.df <- subset(all.df, subset = (all.df$nc.id %in% coverage.data$nc))
-
-
+#function returns length of genome with id=nc.id
 GetGenLen <- function (nc.id=NULL) {
     genlen.subset <- subset(genlen.all, subset=(genlen.all$V1==nc.id));
     genlen<-genlen.subset$V2
     return(genlen)
 }
 
+#functon returns coordinates of feature from given .ptt file
 GetPTTFile <- function (ptt.file = NULL, product = NULL){
     ptts <- try(fread(input=ptt.file))
     if ( any(class(ptts) == "try-error") ) {
         ptts <- NULL
-        cat(paste(Sys.time(), "error in PTT reading!: ",ptt.file, sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)  
+        cat(paste(Sys.time(), "ERROR! in PTT reading!: ",ptt.file, sep="\t"), file=log.file, sep="\n", append=T)  
     }else{
         ptt.product.pattern <- paste0("*",product)
         ptts<-subset(x=ptts, grepl(ptt.product.pattern, ptts$Product))
         
+        #Separate string starts..ends to starts and ends
         list.tmp <- strsplit(as.character(ptts$Location), "\\..")
         StartEnd <- ldply(list.tmp)
+        
         if(nrow(StartEnd)>0) colnames(StartEnd) <- c("Start", "End")
         
         ptts$Start <- as.numeric(StartEnd$Start)
         ptts$End <- as.numeric(StartEnd$End)
         ptts$Size <- as.numeric(ptts$End-ptts$Start)
         
-        ptts <- subset(ptts, ptts$Size>0)
+        ptts <- subset(ptts, ptts$Size>0) #filter 0-sized features
         class(ptts)<- "data.frame"
         
     }
@@ -75,63 +85,67 @@ GetPTTFile <- function (ptt.file = NULL, product = NULL){
 }
 
 pval.find <-function (nc.id=NULL, ptt.file=NULL, pal.file=NULL){
-    ###logging###
-    cat(paste(Sys.time(), "start: ",nc.id, ptt.file, pal.file, sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)
-    ###/logging###
+    cat(paste(Sys.time(), "[START:]: ",nc.id, ptt.file, pal.file, sep="\t"), file=log.file, sep="\n", append=T)
+
     PTTs <- GetPTTFile(ptt.file,feature)
     genlen <- GetGenLen(nc.id)
     PALs <- try(fread(input = pal.file, sep = "\t"))
     
     if ( any(class(PALs)!="try-error" ) & any(class(PTTs) != "NULL") & nrow(PTTs)>0){
-        PALs.IR <- reduce(IRanges(start=PALs$V1, end=PALs$V2))
+        pals.ir <- reduce(IRanges(start=PALs$V1, end=PALs$V2))
         
-        ##MAybe there's an ERROR!
-        leftside <- IRanges(start=PTTs$Start-50, end=PTTs$Start)
-        rightside <-IRanges(start=PTTs$End, end = PTTs$End+50)
-        PTTs.IR <-reduce(c(leftside, rightside)) 
+        #Get flanks of feature from .ptt file
+        leftside <- IRanges(start=PTTs$Start-flank_length, end=PTTs$Start)
+        rightside <-IRanges(start=PTTs$End, end = PTTs$End+flank_length)
+        flanks.ir <-reduce(c(leftside, rightside)) 
         
         
-        hits.real <- sum(countOverlaps(query = PALs.IR, subject = PTTs.IR))
+        hits.real <- sum(countOverlaps(query = pals.ir, subject = flanks.IR))
         pval <- 0
         
-        maxcoord<-genlen - (max(PTTs.IR@width)+50)
-        reps <- length(PTTs.IR)
+        maxcoord<-genlen - (max(PTTs$Size)+flank_length)
+        reps <- nrow(PTTs)
         if (reps >0 & maxcoord >0){
             for (i in 1:1000){
                 start.rnd <- sort(sample(maxcoord,reps))
                 end.rnd <-start.rnd+PTTs$Size
                 
-                #PTTs.IR УЖЕ содержат фланки, следовательно, сейчас мы сгенерим в 2 раза больше отрезков
-                leftside <- IRanges(start=start.rnd-50, end=start.rnd)
-                rightside <-IRanges(start=end.rnd, end = end.rnd+50)
-                PTTs.rand.IR <-reduce(c(leftside, rightside))
                 
-                hits.rand <-sum(countOverlaps(query=PALs.IR, subject=PTTs.rand.IR))
+                leftside <- IRanges(start=start.rnd-flank_length, end=start.rnd)
+                rightside <-IRanges(start=end.rnd, end = end.rnd+flank_length)
+                flanks.rand.ir <-reduce(c(leftside, rightside))
+                
+                hits.rand <-sum(countOverlaps(query=pals.ir, subject=flanks.rand.ir))
                 if (hits.rand >= hits.real) pval=pval+1
             }
             
         }else{
-            cat(paste(Sys.time(), "error in reps or maxcoord: ",nc.id, ptt.file, pal.file, sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)    
+            cat(paste(Sys.time(), "ERROR! in reps or maxcoord: ",nc.id, ptt.file, pal.file, sep="\t"), file=log.file, sep="\n", append=T)    
             pval = -1
         }   
     }else{
-        cat(paste(Sys.time(), "PTT or PAL err: ",nc.id, ptt.file, pal.file, sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)
+        cat(paste(Sys.time(), "ERROR! in reading .pal or empty PTT: ",nc.id, ptt.file, pal.file, sep="\t"), file=log.file, sep="\n", append=T)
         pval= -2
     }
-    cat(paste(Sys.time(), "result: ",nc.id, pval, sep="\t"), file=paste0(work.env, "_", feature,"_flank50_PALvsPTT.results.txt"), sep="\n", append=T)
+    cat(paste(Sys.time(), "result: ",nc.id, pval, sep="\t"), file=out.file, sep="\n", append=T)
     return(pval)
 }
 
-cat(paste(Sys.time(), "Making cluster!", sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)
+
+cat(paste(Sys.time(), "Making cluster!", sep="\t"), file=log.file, sep="\n", append=T)
 cl.local<-makeCluster(getOption("cl.cores", 3))
-clusterExport(cl=cl.local, varlist=c("all.df", "genlen.all", "pval.find", "GetGenLen","GetPTTFile", "work.env", "feature"))
+clusterExport(cl=cl.local, varlist=c("all.df", "genlen.all", "pval.find", "GetGenLen","GetPTTFile", 'log.file','out.file', "feature"))
 clusterEvalQ(cl.local,require(IRanges))
 clusterEvalQ(cl.local,require(data.table))
 clusterEvalQ(cl.local,require(plyr))
 all.df$rslts<-parApply(cl.local, all.df[,c('nc.id','ptt.file','pal.file')], 1, function(x) pval.find(x[1],x[2],x[3]))
 stopCluster(cl.local)
-cat(paste(Sys.time(), "Stopping Cluster", sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)
+cat(paste(Sys.time(), "Stopping Cluster", sep="\t"), file=log.file, sep="\n", append=T)
 
-write.table(x<-all.df, file=paste0(work.env, "_", feature,"_flank50_PALvsPTT.results2.txt"), sep="\t")
+write.table(x<-all.df, file=all.file, sep="\t")
 
-cat(paste(Sys.time(), "DONE!", sep="\t"), file=paste0(work.env, "_", feature, "_flank50_PALvsPTT.log"), sep="\n", append=T)
+cat(paste(Sys.time(), "DONE!", sep="\t"), file=log.file, sep="\n", append=T)
+
+
+
+
